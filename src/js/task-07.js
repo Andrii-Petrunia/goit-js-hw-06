f37e958153cc96dc62152ce3075eb75287fd2afa
Andrii-Petrunia <petrunyandrey@gmail.com>
2023-10-08 08:47:20 +0200

    com-06-2

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,5 +1,5 @@
 const cnangeFontSize = document.querySelector("#font-size-control");
-const textElement = document.querySelector("#text")a
+const textElement = document.querySelector("#text");
 cnangeFontSize.addEventListener("input", () => {
   const fontSize = cnangeFontSize.value + "px";
   textElement.style.fontSize = fontSize;
